ed65ae1ac4b653898eb6131f92bc421bbb3d8b81
sayda <sayda>
2017-04-04 12:45:35 -0500

    animacion de texto

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function init(){
     $('#habilidades').click(navegaHabilidades);
     $('#proyectos').click(navegaProyectos);
     $('#contactame').click(naveContactame);
-    $('h1').addClass('animated infinite pulse');
+   
 }
 //navegar a seciones
 function navegaInicio(){
@@ -44,7 +44,14 @@ function naveContactame(){
     $('#secHabilidades').hide(1000);
     $('#secInicio').hide(1000);
     $('#sobreMi').hide(1050);
+    $('.fondoContactame').addClass('animated infinite pulse');
 }
-function agregaAnimacion(){
-    
-}
+document.addEventListener('DOMContentLoaded',function(event){ 
+    var dataText = ["Sayda Quintanilla","Sayda Quintanilla","Sayda Quintanilla","Sayda Quintanilla"];
+ function typeWriter(text, i, fnCallback){ 
+ if (i < (text.length)) {
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>'; setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback) }, 100); } else if (typeof fnCallback == 'function') {
+            setTimeout(fnCallback, 700); 
+     } } 
+ function StartTextAnimation(i) { if (typeof dataText[i] == 'undefined'){ setTimeout(function() { StartTextAnimation(0); }, 20000); } if (i < dataText[i].length) { typeWriter(dataText[i], 0, function(){ StartTextAnimation(i + 1); }); } } StartTextAnimation(0); });
