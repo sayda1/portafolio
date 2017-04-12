fc84d5b0132b442161d5a0a54b43f29d8bc2b676
sayda <sayda>
2017-04-12 12:16:59 -0500

    animacion

--- js/main.js
+++ js/main.js
@@ -53,5 +53,6 @@ document.addEventListener('DOMContentLoaded',function(event){
      document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>'; setTimeout(function() {
         typeWriter(text, i + 1, fnCallback) }, 100); } else if (typeof fnCallback == 'function') {
             setTimeout(fnCallback, 700); 
-     } } 
- function StartTextAnimation(i) { if (typeof dataText[i] == 'undefined'){ setTimeout(function() { StartTextAnimation(0); }, 20000); } if (i < dataText[i].length) { typeWriter(dataText[i], 0, function(){ StartTextAnimation(i + 1); }); } } StartTextAnimation(0); });
+     } 
+ } 
+ function StartTextAnimation(i) { if (typeof dataText[i] == 'undefined'){setTimeout(function() { StartTextAnimation(0); }, 20000); } if (i < dataText[i].length) { typeWriter(dataText[i], 0, function(){ StartTextAnimation(i + 1); }); } } StartTextAnimation(0); });
